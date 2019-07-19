668b54418731e981d05d767a8d23ab6f78697dc9
martingoiriz <martingoiriz@gmail.com>
2019-07-19 12:23:58 -0300

    Fix formulario

--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -1,33 +1,45 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Http, Headers, RequestOptions } from '@angular/http'
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Http, Headers, RequestOptions } from "@angular/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ServicesService {
-  urlReference:any;
+  urlReference: any;
 
-  constructor(private  httpClient:HttpClient,private httpPost: Http) { 
-    this.httpClient.get('assets/data/referencias.json').subscribe(url=>{
-      this.urlReference = url['url'];
-      console.log('reference', this.urlReference);
-    })
+  constructor(private httpClient: HttpClient, private httpPost: Http) {
+    this.httpClient.get("assets/data/referencias.json").subscribe(url => {
+      this.urlReference = url["url"];
+      console.log("reference", this.urlReference);
+    });
   }
 
   sendSorteoData(data): Observable<any> {
-    console.log('llego a la API', data);
+    console.log("llego a la API", data);
+
     var headers = new Headers();
-    headers.append('Access-Control-Allow-Origin', '*');
-    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
-    headers.append('Accept', 'application/json');
-    headers.append('content-type', 'application/json');
+
+    headers.append("Access-Control-Allow-Origin", "*");
+    headers.append("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
+    headers.append("Accept", "application/json");
+    headers.append("content-type", "application/json");
     const requestOptions = new RequestOptions({ headers: headers });
 
+    var body = JSON.stringify({
+      nombreYapellido: data.firstName,
+      mail: data.email,
+      ciudad: data.ciudad,
+      provincia: data.provincia,
+      moto: data.moto,
+      productoMP: data.productoMP
+    });
 
-    var body = JSON.stringify({ nombreYapellido: data.nomApe, mail: data.email, ciudad: data.ciudad, provincia: data.provincia, moto: data.moto, productoMP: data.productoMP });
     console.log(body);
-    return this.httpPost.post(this.urlReference+"guardarencuesta.php", body, { headers: headers, withCredentials: true });
+    return this.httpPost.post(this.urlReference + "guardarencuesta.php", body, {
+      headers: headers,
+      withCredentials: true
+    });
   }
 }
--- src/app/sorteo/sorteo.component.ts
+++ src/app/sorteo/sorteo.component.ts
@@ -1,41 +1,50 @@
-import { ServicesService } from './../services.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ServicesService } from "./../services.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'abe-sorteo',
-  templateUrl: './sorteo.component.html',
-  styleUrls: ['./sorteo.component.scss']
+  selector: "abe-sorteo",
+  templateUrl: "./sorteo.component.html",
+  styleUrls: ["./sorteo.component.scss"]
 })
 export class SorteoComponent implements OnInit {
-  data:any=[];
+  data: any = [];
 
   model: any = {};
-  constructor(
-    private router: Router,
-    private services: ServicesService,
-  ) { }
+  constructor(private router: Router, private services: ServicesService) {}
 
-  
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(form: NgForm) {
+    this.model = form.value;
+
+    if (
+      this.model.firstName &&
+      this.model.email &&
+      this.model.ciudad &&
+      this.model.provincia &&
+      this.model.moto &&
+      this.model.productoMP
+    ) {
+      this.services.sendSorteoData(this.model).subscribe(data => {
+        console.log("vuelta", data);
+        this.data = JSON.parse(data["_body"])["data"];
+        console.log("data", this.data);
+
+        if (this.data) {
+          console.log("entroAlIf");
+
+          this.router.navigate(["/", "a"]).then(
+            nav => {
+              console.log(nav); // true if navigation is successful
+            },
+            err => {
+              console.log(err); // when there's an error
+            }
+          );
+        }
+      });
+    }
   }
-
-  sendSorteo(){
-    console.log(this.model);
-    this.services.sendSorteoData(this.model).subscribe(data=>{
-      console.log('vuelta', data);
-      this.data = JSON.parse(data['_body'])['data'];
-      console.log('data', this.data);
-      if(this.data){
-        console.log('entroAlIf')
-        this.router.navigate(['/', 'a']).then(nav => {
-          console.log(nav); // true if navigation is successful
-        }, err => {
-          console.log(err) // when there's an error
-        });
-      }
-    });
-  }
-
-
 }
